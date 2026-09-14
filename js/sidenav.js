4b00af42aabf059b1330a8ac429caa4578d89d12
nurulalamador <nurulalomador@gmail.com>
2026-09-14 20:35:57 +0600

    portfolio updated

--- js/sidenav.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("mySidenav").classList.remove("menuoff");
-  document.getElementById("mySidenav").classList.add("menuon");
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").classList.add("menuoff");
-  document.getElementById("mySidenav").classList.remove("menuon");
-}
---
+++ old/js/sidenav.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").classList.remove("menuoff");
+  document.getElementById("mySidenav").classList.add("menuon");
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").classList.add("menuoff");
+  document.getElementById("mySidenav").classList.remove("menuon");
+}
